505a2e686a8226a808e77d6108ee3ed5cdb158e1
Robbie Holmes <robbiethegeek@gmail.com>
2016-06-23 14:01:35 -0400

    add more output

--- test/first.spec.js
+++ test/first.spec.js
@@ -7,5 +7,7 @@ const addTwoNumbers = (num1, num2) => {
 var three = addTwoNumbers(1, 2);
 console.log("Assert that adding 1,2 is 3");
 assert.equal(three, 3);
+console.log("1 + 2 does equal 3");
 console.log("Assert that adding 1,2 is not 4");
 assert.notEqual(three, 4);
+console.log("1 + 2 does not equal 4");
